9d13f87ef0afc5e6be42fb99563cd3399db56d12
farshid keshavarzian <farshidkmb@gmail.com>
2022-06-23 08:37:58 -0700

    show data in table

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { serviceApi } from "../services/serviceApi";
+import styles from "./Home.module.css"
 
 function Home(){
     const [data, setData] = useState([]);
@@ -7,13 +8,41 @@ function Home(){
         console.log("Home Mounted");
         serviceApi.GET(
             "/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-        ).then((result)=>{console.log(result)});
+        ).then((result)=>{
+            if(result.status === 200){
+                console.log(result.data)
+                setData(result.data);
+            }
+        });
 
     },[]);
 
     return(
         <>
-            home
+            <table className={styles.mainTable}>
+                <thead>
+                    <tr>
+                        <th>Icon</th>
+                        <th>Name</th>
+                        <th>Acroname</th>
+                        <th>Price</th>
+                        <th>Change</th>
+                        <th>Market CAP</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((d)=>{return(
+                        <tr key = {d.id}>
+                            <td><img src={d.image}></img></td>
+                            <td>{d.name}</td>
+                            <td>{d.symbol}</td>
+                            <td>{d.current_price}</td>
+                            <td>{d.price_change_percentage_24h}</td>
+                            <td>{d.market_cap}</td>
+                        </tr>
+                        )})}
+                </tbody>
+            </table>
         </>
     )
 }
